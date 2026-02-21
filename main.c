06672bc00ea4198ec9bc05ef2b057458ae44ca48
Handara <rachidhandaoui@gmail.com>
2026-02-21 13:39:34 +0000

    Add notes to staff. for not it's in the  drawStaff method but I should use frames to make the slide on the staff.

--- main.c
+++ main.c
@@ -4,14 +4,16 @@
 #define WINDOW_HEIGHT GetScreenHeight()
 #define WINDOW_WIDTH GetScreenWidth()
 #define G_CLEF_CODEPOINT 0xE050
+#define NOTE_CODEPOINT 0xE1D5
 
-float fontSize = 100;
+float fontSize = 200;
 int staffWidth = 30;
 
 int musicCodepoints[] = { 
         G_CLEF_CODEPOINT, // G-clef
         0xE05C, // C-clef
-        0xE062  // F-clef
+        0xE062,  // F-clef
+        NOTE_CODEPOINT // Single note
     }; 
 
 float topStaffPercent = 3.0f/32.0f;
@@ -20,21 +22,26 @@ float bottomStaffPercent = 10.85f/32.0f;
 void drawStaff(Font bravura, Vector2 position, int staffTopPosY, int staffWidth){
     DrawTextCodepoint(bravura, G_CLEF_CODEPOINT,position, fontSize, BLACK );
     // printf("staffTopPosY %d staffBottomPosY %d staffWidth %d", staffTopPosY,staffBottomPosY,staffWidth);
+    DrawRectangleLines((int)position.x+150, (int)staffTopPosY-2*staffWidth, staffWidth*2, staffWidth*8, RED);
     for (int i=0; i < 5; i++){
         
         DrawLine((int)position.x, (int)staffTopPosY+i*staffWidth, (int)position.x+800, (int)staffTopPosY+i*staffWidth, BLACK);
+
+
     }
+     for (int i=0; i < 12; i++){
+        DrawTextCodepoint(bravura, NOTE_CODEPOINT,(Vector2){position.x+50+(50*i),(float)position.y-3*staffWidth+i*(staffWidth/2)}, fontSize, BLACK );
+    }
+
 }
 
 int main(void){
     InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "HELLO WORLD!");
     // SetTextureFilter(bravura.texture, TEXTURE_FILTER_BILINEAR);
-    Font bravura = LoadFontEx("Ressources/Bravura.otf", (int)fontSize, musicCodepoints, 3);
+    Font bravura = LoadFontEx("Ressources/Bravura.otf", (int)fontSize, musicCodepoints, 4);
     
-    printf("topStaffPercent %f bottomStaffPercent %f",topStaffPercent,bottomStaffPercent);
     Vector2 staffPosition = {200,300};
-    printf("bravura glyph offsetY%d\n",bravura.glyphs->offsetY);
-    printf("bravura glyph offsetX%d\n",bravura.glyphs->offsetX);
+    
     int staffTopPosY = (int)staffPosition.y+bravura.glyphs->offsetY + (int)(fontSize*topStaffPercent);
     int staffBottomPosY = (int)staffPosition.y+bravura.glyphs->offsetY + (int)(fontSize*bottomStaffPercent);
     int staffWidth = (staffBottomPosY - staffTopPosY)/4;
